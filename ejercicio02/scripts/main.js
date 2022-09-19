68ed020a60107b04d4f758308b7bb15ca2ece714
Iminguet <i.minguet@hotmail.com>
2022-09-20 00:31:25 +0200

    ejercicio 2 terminado

--- ejercicio02/scripts/main.js
+++ ejercicio02/scripts/main.js
@@ -1,1 +1,27 @@
-'use-strict'
+'use-strict';
+
+const userCantidadNumeros = parseInt(
+  prompt('¿Cuantos números aleatorios quieres generar?')
+);
+
+const valor01 = parseInt(
+  prompt('¿Entre que valores?, introduce el primer valor...')
+);
+
+const valor02 = parseInt(prompt('...introduce el segundo valor'));
+
+const numerosRandom = function (min, max) {
+  return parseInt(Math.random() * (max - min + 1) + min);
+};
+
+const llenarMostrarArray = function (cantidadIteraciones, minimo, maximo) {
+  const arrayNumeros = [];
+  let numeros = '';
+  for (let i = 0; i < cantidadIteraciones; i++) {
+    numeros = numerosRandom(minimo, maximo);
+    arrayNumeros.push(numeros);
+  }
+  console.log(arrayNumeros);
+};
+
+llenarMostrarArray(userCantidadNumeros, valor01, valor02);
